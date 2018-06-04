829a20dc03512f6c2f5c1f23d93ab59db62b4d34
suemi94 <suemi94@qq.com>
2018-06-04 14:21:46 +0800

    do some

--- Install/install_app.sh
+++ Install/install_app.sh
@@ -12,3 +12,22 @@ pacman -S i3scrot
 
 # 安装文档查阅软件
 yaourt -S zeal
+
+# touchpad
+pacman -S xf86-input-libinput
+sudo pacman -S xorg-xinput
+vim /usr/share/X11/xorg.conf.d/40-libinput.conf
+
+pacman -S gnome-disk-utility
+
+# 时钟同步
+timedatectl set-ntp true
+
+#输入法
+pacman -S fcitx fcitx-configtool
+# 在i3和xfce下安装搜狗输入法需要额外安装
+pacman -S fcitx-qt
+pacman -S fcitx-sogoupinyin
+
+
+pacman -S foxitreader wps-office
